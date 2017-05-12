1e8e819d57c7b0031405ca2a3ba9242821ab9ab3
Jeremy Kisner <jkisner984@gmail.com>
2017-05-12 17:28:25 -0400

    Add files via upload

---
+++ function.js
@@ -0,0 +1,25 @@
+var zipcode;
+function getZip(zipcode){
+	
+	if (zipcode > 20000){
+		return true;
+	}
+	
+	else{
+		return false;
+	}
+	
+}
+
+
+
+function createBanner(){
+	
+	var ban = document.getElementById("banner");
+	var info = document.createElement("h2");
+		
+	info.innerHTML = "Banner Information";
+		
+	ban.appendChild(info);
+}
+
