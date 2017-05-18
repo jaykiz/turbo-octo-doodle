024d36c0f41ffd48d07c19d3de574c13af47a02d
Jeremy Kisner <jkisner984@gmail.com>
2017-05-18 02:21:58 -0400

    Add files via upload

--- function.js
+++ function.js
@@ -11,32 +11,56 @@ function getZip(zipcode){
 	
 }
 
-
-
-function createBanner(){
 	
-	var ban = document.getElementById("banner");
-	var info = document.createElement("h2");
+}
+
+var style = 0;
+function calculateStyle(value){
+	var styleType = document.getElementById("style");
+	var x = value;
+	
+	
+		if(x=="classic"){
+			style =3.00;
+		}
 		
-	info.innerHTML = "Banner Information";
+		else if(x=="premium"){
+			style=5.00;
+		}
 		
-	ban.appendChild(info);
-}
-
-
-function calculateStyle(style){
-	
+		else if(x=="royal"){
+			style=10.00;
+		}
+		
 	
 }
 
 
-function calculateTip(tip){
+var tip =0;
+function calculateTip(value){
+	var tipType = document.getElementById("tip");
+	var y = value;
 	
+	if(y == "10"){
+		tip=0.10;
+	}
+	else if(y=="15"){
+		tip=0.15;
+	}
+	else if(y=="20"){
+		tip=0.20;
+	}
 	
+	calculateTotal(style, tip);
 }
 
 
-function calculateTotal(){
+function calculateTotal(style, tip){
+	var temp= style*tip;
+	var total = style+temp;
 	
-	
+	var totalDiv=document.getElementById("total");
+	var totalHeader = document.createElement("h2");
+	totalHeader.innerHTML = "Total: "+total;
+	totalDiv.appendChild(totalHeader);
 }
